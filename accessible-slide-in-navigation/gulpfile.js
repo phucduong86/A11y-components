1d6db29286064538e0d2c718d0291c2a890af37b
phucduong86 <phuc.duongdinh@gmail.com>
2019-04-10 21:40:57 -0400

    first component

---
+++ accessible-slide-in-navigation/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series(['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./*.scss", gulp.series('sass'));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+}));
+
+
+
+gulp.task('default', gulp.series('serve'));
